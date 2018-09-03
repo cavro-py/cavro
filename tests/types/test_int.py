db86582c2ba0b068aacd3e196afb0073f29c603d
Steve Stagg <stestagg@gmail.com>
2018-09-03 01:07:58 +0100

    Add some tests for long encoding

--- tests/types/test_int.py
+++ tests/types/test_int.py
@@ -15,3 +15,40 @@ import pytest
 def test_int_encoding(value, expected):
     schema = cavro.Schema('"int"')
     assert schema.binary_encode(value) == expected
+
+
+def test_int_overflow():
+    schema = cavro.Schema('"int"')
+    assert schema.can_encode(2**32) == False
+    with pytest.raises(OverflowError) as exc:
+        schema.binary_encode(2**33)
+    assert "value too large" in str(exc.value)
+
+
+@pytest.mark.parametrize("value,expected", [
+    (0, b'\x00'),
+    (-1, b'\x01'),
+    (1, b'\x02'),
+    (-2, b'\x03'),
+    (2, b'\x04'),
+    (-64, b'\x7f'),
+    (64, b'\x80\x01'),
+    ((2**31)-1, b'\xfe\xff\xff\xff\x0f'),
+    (-(2**31), b'\xff\xff\xff\xff\x0f'),
+    ((2**63)-1, b'\xfe\xff\xff\xff\xff\xff\xff\xff\xff\x01'),
+    (-(2**63), b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01'),
+])
+def test_long_encoding(value, expected):
+    schema = cavro.Schema('"long"')
+    assert schema.binary_encode(value) == expected
+
+
+def test_long_overflow():
+    schema = cavro.Schema('"long"')
+    assert schema.can_encode(2**64) == False
+    with pytest.raises(OverflowError) as exc:
+        schema.binary_encode(2**65)
+    assert "too large" in str(exc.value)
+    with pytest.raises(OverflowError) as exc:
+        schema.binary_encode(-2**65)
+    assert "too large" in str(exc.value)
